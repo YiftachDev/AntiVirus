8de94382cb66285b7d4ae32ad2156760ec8fdcdb
Yiftach <yiftach007@gmail.com>
2024-10-22 23:43:07 +0300

    Created 'create_message' function to create the client message

--- client/client.py
+++ client/client.py
@@ -10,10 +10,22 @@ import protocol
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 2223
 
+def create_message(data):
+    temp_arr = protocol.create_file_data_list(data)
+    temp_arr.sort(key=lambda doc: doc.get("fileName"))
+    msg = ""
+    for i in range(len(temp_arr)):
+        msg += temp_arr[i]["fileName"] + ":" + temp_arr[i]["hash"] 
+        if i != len(temp_arr) - 1:
+            msg += ','
+    msg = protocol.add_length_to_message(msg)
+    return msg
+
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_IP, SERVER_PORT))
-client_msg = "testfile:awdasd,testfile2:fghfgsdff,testfile3:hrty,testfile4:rrswerf"
-client_msg = protocol.add_length_to_message(client_msg)
+client_data = "atest-file2:test-hash2,test-file:fghfgsdff"
+client_msg = create_message(client_data)
 client_socket.send(client_msg.encode())
 server_response = protocol.get_msg(client_socket)
 print(server_response)
